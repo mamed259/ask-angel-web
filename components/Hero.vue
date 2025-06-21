7ee5b5591e0135c4fa283c153690c3d1f092545f
Elmir's Mac mini <elmirmammadli18@gmail.com>
2025-06-21 08:18:55 -0400

    Add hero section components with collage design

---
+++ components/Hero.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col items-center justify-center py-[80px] overflow-hidden 2xl:overflow-visible">
+    <div class="relative max-w-[1352px] mx-auto text-center">
+      <div class="max-w-4xl mx-auto">
+        <Heading variant="h1" class="mb-8">
+          Get Your Lifetime<br>
+          Health<NuxtIcon name="heart" filled class="inline-block w-16 h-16 mx-4" /> Companion
+        </Heading>
+        
+        <p class="text-xl font-sans text-primary max-w-3xl mx-auto leading-relaxed mb-12 px-12">
+          From sleep to stress, movement to mindset—our app puts every 
+          piece&nbsp;of&nbsp;your health puzzle into context to help you achieve your 
+          goals&nbsp;and&nbsp;live a healthier life.
+        </p>
+        
+        <button class="bg-primary cursor-pointer text-background font-sans font-semibold text-lg px-12 py-5 rounded-full transition-colors duration-300 uppercase tracking-wide">
+          I WANT THE APP
+        </button>
+      </div>
+    <NuxtImg src="/images/hero-icon-research.svg" alt="Hero Icon" class="absolute bottom-9 -left-[30%] w-[125px]" />
+    <NuxtImg src="/images/hero-icon-availability.svg" alt="Hero Icon" class="absolute top-[30%] -right-[35%] w-[184px]" />
+    </div>
+    <HeroCollage />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ components/HeroCollage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex items-center justify-center gap-x-48 py-40 overflow-hidden 2xl:overflow-visible relative left-0 2xl:left-0">
+        <NuxtImg 
+          width="327" 
+          height="709" 
+          src="/images/hero-angel-sad.png" 
+          alt="Hero Image" 
+          class="rotate-10 w-[327px] h-[709px]"
+        />
+        <NuxtImg 
+          width="393" 
+          height="852" 
+          src="/images/hero-sun-happy.png" 
+          alt="Hero Image" 
+          class="w-[393px] h-[852px]"
+        />
+        <NuxtImg 
+          width="360" 
+          height="782" 
+          src="/images/hero-angel-happy.png" 
+          alt="Hero Image"
+          class="-rotate-8 relative top-[120px] w-[360px] h-[782px]"
+        />
+      </div>
+</template>
