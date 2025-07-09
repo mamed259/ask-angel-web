f23e503b50abaa7280fd88721cefcbe2e7b2713a
Elmir's Mac mini <elmirmammadli18@gmail.com>
2025-07-09 09:15:46 -0400

    feat: Enhance index.vue with new layout and content for coaching section
    - Replace multiple line breaks with a structured layout for the coaching section
    - Add SectionHeader component for title and subtitle
    - Introduce a new visual element with an angel icon and chat image
    - Include a call-to-action button and descriptive text for user engagement

--- pages/index.vue
+++ pages/index.vue
@@ -61,17 +61,36 @@
 
     <Tabs/>
 
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    <div class="max-w-[1352px] mx-auto my-30">
+      <SectionHeader align="center">
+        <template #title>
+          Meet Your Angel: Coach <br>That trully Understands You
+        </template>
+        <template #subtitle>
+          Real-time guidance. Emotional support. <br>Smarter decisions.
+        </template>
+      </SectionHeader>
+      <div class="flex justify-center gap-x-4 mt-12">
+        <span class="relative">
+          <NuxtIcon name="angel" filled class="absolute -top-24 left-14 text-[270px]" />
+          <img
+          class="rounded-[32px] w-[668px] h-full" 
+          src="/images/chat.png" 
+          alt="chat" />
+        </span>
+        <div class="bg-white w-[668px] h-[485px] p-10 rounded-[32px] flex flex-col justify-between">
+          <button class="bg-primary text-white uppercase font-founders font-semibold text-base px-6 py-3 rounded-full cursor-pointer self-start">
+            start chatting &#8594;
+          </button>
+          <p>
+            Health isn’t static—it’s a dynamic interplay of sleep, stress, nutrition, activity, and mental state. Our predictive engine continuously analyzes your biometric trends, behavior patterns, and contextual data to anticipate shifts in your physical and mental well-being—before symptoms manifest.
+            <br>
+            <br>
+            It’s there to coach, comfort, and clarify—turning information into action, and motivation into momentum. No judgment. Just gentle, informed support that evolves with you.
+          </p>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -82,6 +101,7 @@ import Slider from '../components/Slider.vue';
 import QuoteBlock from '../components/QuoteBlock.vue';
 import Card from '../components/Card.vue';
 import Tabs from '../components/tabs/Tabs.vue';
+import SectionHeader from '../components/SectionHeader.vue';
 
 const quoteContent = {
   quote: "Health is outcome of many factors. We're the first app to unify all its drivers into one intelligent system—for lasting, evidence-based change.",
