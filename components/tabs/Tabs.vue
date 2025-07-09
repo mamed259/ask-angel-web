e62012a5cc353b0253c6f06e23d97f728fa4e6ad
Elmir's Mac mini <elmirmammadli18@gmail.com>
2025-07-09 01:35:21 -0400

    refactor: Enhance Tabs and TabsContent components for improved layout and responsiveness
    - Update Tabs component to adjust width and padding for better alignment
    - Modify TabsContent component to improve image handling and text styling
    - Enhance overall responsiveness and visual hierarchy in tab content

--- components/tabs/Tabs.vue
+++ components/tabs/Tabs.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="max-w-[1352px] mx-auto py-30">
+<div class="min-w-[320px] max-w-[361px] sm:max-w-[624px] xl:max-w-[1352px] mx-auto xl:mx-20 py-30 3xl:mx-auto">
   <Heading variant="h2" class="text-center z-10">
     Sustainable Change <br>One Goal at a Time
   </Heading>
@@ -8,11 +8,11 @@
   </p>
 
   <div class="tabs-container">
-    <ul class="flex flex-wrap gap-4 justify-center mt-10 mb-8">
+    <ul class="flex flex-wrap gap-3 xl:gap-4 justify-center mt-10 mb-8">
       <li
           v-for="(tab, index) in tabs"
           :key="index"
-          :class="['tab-item border-2 border-primary px-6 py-3 font-founders text-sm font-semibold uppercase rounded-3xl transition-all cursor-pointer', { 'bg-primary text-white': selectedTab === index }]"
+          :class="['tab-item border-2 border-primary px-4 xl:px-6 py-2 xl:py-3 font-founders text-sm font-semibold uppercase rounded-3xl transition-all cursor-pointer', { 'bg-primary text-white': selectedTab === index }]"
           @click="selectTab(index as TabUnions)">
         {{ index }}
       </li>
--- components/tabs/TabsContent.vue
+++ components/tabs/TabsContent.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="p-10 bg-white rounded-[32px]">
-      <div class="flex gap-x-[75px]">
+  <div class="p-6 xl:p-10 bg-white rounded-[32px]">
+      <div class="flex flex-col xl:flex-row gap-x-[75px]">
             <img
                 :src="tab[tabKey].image" 
                 alt="tab image" 
-            class="w-[569px] h-[506px] object-contain rounded-[24px]"
+            class="max-w-[569px] w-full h-full rounded-[24px]"
             />
         <div class="flex flex-col justify-center">
-          <Heading variant="h4" class="max-w-[355px] w-full z-10 mb-4">{{ tab[tabKey].title }}</Heading>
-          <p class="max-w-[330px] w-full font-bold mb-6 text-base uppercase font-founders">{{ tab[tabKey].description }}</p>
-          <p class="max-w-[500px] w-full text-lg font-medium font-founders" v-html="tab[tabKey].perex"></p>
+          <Heading variant="h4" class="max-w-[260px] xl:max-w-[355px] w-full z-10 mb-4 mt-8 xl:mt-0">{{ tab[tabKey].title }}</Heading>
+          <p class="max-w-[330px] w-full font-bold mb-6 text-xs xl:text-base uppercase font-founders">{{ tab[tabKey].description }}</p>
+          <p class="max-w-[500px] w-full text-base xl:text-lg font-light font-founders" v-html="tab[tabKey].perex"></p>
         </div>
       </div>
     </div>
