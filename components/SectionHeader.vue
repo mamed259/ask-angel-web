57febf5088e77724ad726425221ba67c782337f0
Elmir's Mac mini <elmirmammadli18@gmail.com>
2025-07-09 09:15:34 -0400

    feat: Add SectionHeader component for flexible alignment and styling
    - Introduce SectionHeader component with customizable alignment options (center, left, right)
    - Implement slots for title and subtitle to enhance content flexibility
    - Utilize Heading component for consistent typography

---
+++ components/SectionHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="['flex flex-col justify-center', { 'items-center': align === 'center', 'items-start': align === 'left', 'items-end': align === 'right' }]">
+    <Heading variant="h2" class="z-10" :class="align === 'left' ? 'text-left' : (align === 'right' ? 'text-right' : 'text-center')">
+    <slot name="title" />
+  </Heading>
+  <p class=" text-primary text-base xl:text-[22px] font-founders font-light" :class="align === 'left' ? 'text-left' : (align === 'right' ? 'text-right' : 'text-center')">
+    <slot name="subtitle" />
+  </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Heading from './Heading.vue';
+
+withDefaults(defineProps<{
+  align?: 'center' | 'left' | 'right';
+}>(), {
+  align: 'center',
+});
+</script>
